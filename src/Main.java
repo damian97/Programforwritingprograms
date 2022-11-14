3fb91d1555962a38ccd0e15181231b84879f1f07
sryncu <sryncu@o2.pl>
2022-11-14 20:53:35 +0100

    Inny notatnik

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Witaj. Napisałem dla Ciebie program, który napisze prgram, który wykona wybraną przez Ciebie czynność.");
         System.out.println("Just for fun ;)");
 
-        System.out.println("start pisanie kodu");
+        String command = "notepad.exe";
+        Runtime run = Runtime.getRuntime();
+        run.exec(command);
+        try {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
 
     }
 }
