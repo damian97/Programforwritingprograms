6b6e8cee17678f63a0773b998d0d39b045b24126
sryncu <sryncu@o2.pl>
2022-11-19 17:25:18 +0100

    Problem ze sprawdzaniem

--- src/Engine.java
+++ src/Engine.java
@@ -1,9 +1,13 @@
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.security.Key;
+import java.util.Scanner;
 
 public class Engine {
 
@@ -77,7 +81,7 @@ public class Engine {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
 
-    }
+        }
 
     public void wklej() {
         bot.delay(100);
@@ -161,6 +165,65 @@ public class Engine {
 
     }
 
+    public void wpiszTab() {
+        bot.keyPress(KeyEvent.VK_TAB);
+        bot.keyRelease(KeyEvent.VK_TAB);
+    }
+
+    public boolean sprawdzLiczbeZmiennej() {
+
+
+        bot.delay(500);
+        bot.keyPress(KeyEvent.VK_CONTROL);
+        bot.keyPress(KeyEvent.VK_A);
+        bot.keyRelease(KeyEvent.VK_A);
+        bot.delay(50);
+        bot.keyPress(KeyEvent.VK_C);
+        bot.keyRelease(KeyEvent.VK_C);
+        bot.keyRelease(KeyEvent.VK_CONTROL);
+        bot.delay(250);
+
+
+
+        Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        DataFlavor dataFlavor = DataFlavor.stringFlavor;
+
+
+        Object text;
+
+        try {
+            text = systemClipboard.getData(dataFlavor);
+        } catch (UnsupportedFlavorException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        String tekst = (String) text;
+
+        String pole = "int area;";
+
+        int dlugoscKodu = tekst.length();
+
+        for (int i = 0, j = 0; i < dlugoscKodu; i++) {
+
+            if (tekst.charAt(i) == pole.charAt(j)) {
+                j++;
+            } else {
+                j = 0;
+            }
+            if (j >= pole.length()-1) {
+                System.out.println(pole.charAt(j));
+            }
+            System.out.print(tekst.charAt(i));
+
+        }
+
+
+        return false;
+    }
+
 
     public void liczPoleTrojkata() {
 
@@ -173,12 +236,25 @@ public class Engine {
         String h = "int h =";
 
         wpiszEnter(1);
+        wpiszTab();
         for (int i = 0; i < pole.length(); i++) {
             bot.keyPress(Character.toUpperCase(pole.charAt(i)));
             bot.delay(50);
         }
 
-//        System.out.println("Pole kwadratu = " + (a*h) / 2);
+
+        while (!sprawdzLiczbeZmiennej()) {
+
+            bot.delay(5000);
+
+        }
+        bot.delay(4000);
+
+
+
+
+
+//        System.out.println("Pole kwadratu = " + (a*h) / 2);    wzor na pole trojkata
 
         zamknijMetody();
 
